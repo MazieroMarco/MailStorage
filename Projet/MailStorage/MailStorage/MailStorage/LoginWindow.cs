8e4db88e4b60814e58197a23cbb771996184e6c6
Marco Maziero <maziero.marco@gmail.com>
2017-05-10 16:33:14 +0200

    - Ajouts dans la documentation - Création de l'interface

---
+++ Projet/MailStorage/MailStorage/MailStorage/LoginWindow.cs
@@ -0,0 +1,67 @@
+﻿/// ETML
+/// Author      : Marco Maziero
+/// Date        : 10.05.2017
+/// Description : Login window, IMAP connect and saves user
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailStorage
+{
+    /// <summary>
+    /// The window main class
+    /// </summary>
+    public partial class LoginWindow : Form
+    {
+        // Window interaction variables
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [System.Runtime.InteropServices.DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        public LoginWindow()
+        {
+            // Initializes all the window components
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Used to move the window when the left click is pressed on the upper bar
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MoveWindow(object sender, MouseEventArgs e)
+        {
+            // When the left button is pressed, moves the window
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        /// <summary>
+        /// Stops the application execution
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ExitApplication(object sender, MouseEventArgs e)
+        {
+            // Exits the application
+            Application.Exit();
+        }
+    }
+}
---
+++ Projet/MailStorage/MailStorage/MailStorage/MailStorage.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace MailStorage
+{
+    partial class MailStorage
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MailStorage));
+            this.barPictureBox = new System.Windows.Forms.PictureBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.mailPictureBox = new System.Windows.Forms.PictureBox();
+            this.minimizeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.barPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mailPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barPictureBox
+            // 
+            this.barPictureBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("barPictureBox.BackgroundImage")));
+            this.barPictureBox.Location = new System.Drawing.Point(0, 0);
+            this.barPictureBox.Name = "barPictureBox";
+            this.barPictureBox.Size = new System.Drawing.Size(700, 50);
+            this.barPictureBox.TabIndex = 0;
+            this.barPictureBox.TabStop = false;
+            this.barPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MoveWindow);
+            // 
+            // exitButton
+            // 
+            this.exitButton.BackColor = System.Drawing.Color.LightCoral;
+            this.exitButton.FlatAppearance.BorderColor = System.Drawing.Color.LightCoral;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exitButton.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.ForeColor = System.Drawing.Color.DarkRed;
+            this.exitButton.Location = new System.Drawing.Point(600, 0);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(100, 50);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "X";
+            this.exitButton.UseVisualStyleBackColor = false;
+            this.exitButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ExitApplication);
+            // 
+            // mailPictureBox
+            // 
+            this.mailPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("mailPictureBox.Image")));
+            this.mailPictureBox.Location = new System.Drawing.Point(20, 0);
+            this.mailPictureBox.Name = "mailPictureBox";
+            this.mailPictureBox.Size = new System.Drawing.Size(90, 50);
+            this.mailPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mailPictureBox.TabIndex = 2;
+            this.mailPictureBox.TabStop = false;
+            // 
+            // minimizeButton
+            // 
+            this.minimizeButton.BackColor = System.Drawing.Color.NavajoWhite;
+            this.minimizeButton.FlatAppearance.BorderColor = System.Drawing.Color.NavajoWhite;
+            this.minimizeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minimizeButton.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minimizeButton.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.minimizeButton.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.minimizeButton.Location = new System.Drawing.Point(530, 0);
+            this.minimizeButton.Name = "minimizeButton";
+            this.minimizeButton.Size = new System.Drawing.Size(70, 50);
+            this.minimizeButton.TabIndex = 4;
+            this.minimizeButton.Text = "-";
+            this.minimizeButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.minimizeButton.UseVisualStyleBackColor = false;
+            // 
+            // MailStorage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(700, 750);
+            this.Controls.Add(this.minimizeButton);
+            this.Controls.Add(this.mailPictureBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.barPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MailStorage";
+            this.Text = "MailStorage";
+            ((System.ComponentModel.ISupportInitialize)(this.barPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mailPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox barPictureBox;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.PictureBox mailPictureBox;
+        private System.Windows.Forms.Button minimizeButton;
+    }
+}
+
